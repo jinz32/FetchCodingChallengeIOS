38d6d2b8da3ad4194c405d723676d20da0291ce4
jinz32 <62317006+jinz32@users.noreply.github.com>
2024-07-15 12:48:51 -0700

    Initial Commit

---
+++ FetchCodingChallengeIOS/FetchCodingChallengeIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchCodingChallengeIOSApp.swift
+//  FetchCodingChallengeIOS
+//
+//  Created by Jonathan Zheng on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchCodingChallengeIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
